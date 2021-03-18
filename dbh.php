cea1429e9d5b1c3f17f55eead25349f9a091f42f
GalGog <galynna00@gmail.com>
2021-03-18 17:32:21 +0000

    changed database credential

--- dbh.php
+++ dbh.php
@@ -1,8 +1,8 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "dbphpsearch";
+$server = "b4b427caa8d29d ";
+$username = "b4b427caa8d29d ";
+$password = "3d64282e";
+$dbname = "heroku_924af9e8ae2fae1";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
